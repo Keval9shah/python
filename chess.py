64538e011ee586c8dc464b6e90db35a7153bd0fc
Keval <skeval904@gmail.com>
2021-06-22 09:38:44 +0530

    ⬛⬜knight moves chess

--- chess.py
+++ chess.py
@@ -1,4 +1,3 @@
-import itertools as it
 import time
 from termcolor import colored
 
@@ -11,43 +10,64 @@ def n(l):
     return i
 
 
+psblmv = [(-2, -1), (-2, 1), (2, -1), (2, 1),
+          (-1, -2), (-1, 2), (1, -2), (1, 2)]
+
+array = []
+
+
 def chess(s, d):
+    global start
+    global xx, yy
     start = time.time()
+    if d > s:
+        s, d = d, s
+    global array
+    global dvsbl
+    global dst
     dvsbl = max(n(s), n(d), 8)
+    # 1st is | & 2nd is --
     src = [s//dvsbl, s % dvsbl]
-    ns = src.copy()
     dst = [d//dvsbl, d % dvsbl]
-    qaz = 0
-    psblmv = list(it.product([-2, 2], [-1, 1])) + \
-        list(it.product([-1, 1], [-2, 2]))
-    for x in range(1, 11):
-        for i in it.product(psblmv, repeat=x):
-            for j in range(len(i)):
-                if ns[0]+i[j][0] > -1 and ns[1]+i[j][1] > -1 and ns[0]+i[j][0] < dvsbl and ns[1]+i[j][1] < dvsbl:
-                    ns[0] += i[j][0]
-                    ns[1] += i[j][1]
-                    if ns == dst:
-                        mids = []
-                        tem = src.copy()
-                        mids.append((src[0]*dvsbl+src[1]))
-                        for c in i:
-                            tem[0] += c[0]
-                            tem[1] += c[1]
-                            mids.append((tem[0]*dvsbl+tem[1]))
-                        print("steps --> ", mids)
-                        for k in range(dvsbl):
-                            for j in range(dvsbl):
-                                qaz = k*dvsbl+j
-                                if qaz in mids:
-                                    print(colored(qaz, 'green',
-                                                  attrs=['bold']), end="\t")
-                                else:
-                                    print(qaz, end="\t")
-                            print()
-                        print("--- ", (time.time() - start), " seconds ---")
-                        print(x, "moves")
-                        return
-            ns = src.copy()
+    if src[0]-dst[0] >= 0 and src[1]-dst[1] >= 0:
+        pass
+    else:
+        temp = src[1]
+        src[1] = dst[1]
+        dst[1] = temp
+    array = [[100 for i in range(dvsbl)] for j in range(dvsbl)]
+    array[src[0]][src[1]] = 0
+    while array[dst[0]][dst[1]] >= 100:
+        addval(src, dst, dvsbl)
+    return array[dst[0]][dst[1]]
 
 
-chess(61, 10)
+def addval(src, dst, dvsbl):
+    global array
+    for i in range(min(dst[0]-1, src[0]+2), max(dst[0]-1, src[0]+2)):
+        for j in range(min(dst[1]-1, src[1]+2), max(dst[1]-1, src[1]+2)):
+            minn = 200
+            if i in range(dvsbl) and j in range(dvsbl) and [i, j] != src:
+                for k in range(8):
+                    if i+psblmv[k][0] in range(dvsbl) and j+psblmv[k][1] in range(dvsbl):
+                        nx = array[i+psblmv[k][0]][j+psblmv[k][1]]+1
+                        if nx < minn:
+                            minn = nx
+                array[i][j] = minn
+
+
+print("--->", colored(chess(156, 11), 'blue', attrs=['bold']), "steps")
+
+# for i in range(dvsbl):
+#   for j in range(dvsbl):
+#     if array[i][j]==0:
+#       print(colored(array[i][j], 'blue',attrs=['bold']), end="\t")
+#     elif [i,j]==dst:
+#       print(colored(array[i][j], 'blue',attrs=['bold']), end="\t")
+#     else:
+#       print(array[i][j],end="\t")
+#   print()
+
+# 1st is | & 2nd is --
+
+print("--- ", (time.time() - start), " seconds ---")
